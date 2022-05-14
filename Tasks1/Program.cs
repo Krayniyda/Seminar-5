ad3d09215dbae30cffe438cd3d41521ad66ade20
KrayniyDA <Krayniy19730507@gmail.com>
2022-05-15 01:34:47 +0700

    first commit

---
+++ Tasks1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Задайте массив, заполненный случайными положительными трёхзначными числами. ");
+Console.WriteLine("Напишите программу, которая покажет количество чётных чисел в массиве.");
+
+Console.Write("Введите количество элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(""); 
+int counter = 0;
+
+Console.Write("Массив, заполненный случайными положительными трёхзначными числами: ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("[");
+int[] array = new int[length];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 1000);
+    Console.Write(array[i]); if (i < length - 1 ) Console.Write("; ");
+    if (array[i] % 2 == 0) counter +=1;
+}
+Console.WriteLine("]"); Console.WriteLine(""); Console.ForegroundColor = ConsoleColor.White;
+Console.Write("Количество чётных чисел в массиве: ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(counter);
+Console.ForegroundColor = ConsoleColor.White;
+    
