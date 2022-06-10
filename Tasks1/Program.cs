5455d4e1e8a49586290f495f94e138edc4e24a07
KrayniyDA <Krayniy19730507@gmail.com>
2022-06-11 06:43:01 +0700

    first commit

--- Tasks1/Program.cs
+++ Tasks1/Program.cs
@@ -1,29 +1,41 @@
-﻿Console.Clear();
-Console.WriteLine("Задайте массив, заполненный случайными положительными трёхзначными числами. ");
-Console.WriteLine("Напишите программу, которая покажет количество чётных чисел в массиве.");
+﻿Random rnd = new Random();
+int M = rnd.Next(1, 21); 
+int N = rnd.Next(1, 21);
 
-Console.Write("Введите количество элементов массива: ");
-int length = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(""); 
-int counter = 0;
-
-Console.Write("Массив, заполненный случайными положительными трёхзначными числами: ");
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.Write("[");
-int[] array = new int[length];
-Random rand = new Random();
-for (int i = 0; i < array.Length; i++)
+textTask(); 
+ 
+if (M > N) 
 {
-    array[i] = rand.Next(100, 1000);
-    Console.Write(array[i]);
-    if (i < length - 1) Console.Write("; ");
-    if (array[i] % 2 != 0)
-        continue;
-    counter += 1;
+    Console.Write($"M = {M}; N = {N}. -> "); Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"M > N, the solution is impossible. "); Console.ResetColor();
+    while (M > N) 
+    {
+        M = rnd.Next(1, 11); 
+        N = rnd.Next(1, 11);
+    } 
 }
-Console.WriteLine("]"); Console.WriteLine(""); Console.ForegroundColor = ConsoleColor.White;
-Console.Write("Количество чётных чисел в массиве: ");
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine(counter);
-Console.ForegroundColor = ConsoleColor.White;
+
+Console.Write($"M = {M}; N = {N}. -> "); Console.ForegroundColor = ConsoleColor.Green;
+PrintNumbers3(M, N);
+if (M == N && M % 3 != 0) Console.WriteLine($"no numbers. ");
+Console.WriteLine("\n"); Console.ResetColor(); Console.ReadKey();
+
+void PrintNumbers3(int i, int n)
+{   if (i < n + 1) 
+    {
+        if (i % 3 == 0) 
+        {
+        Console.Write(i); if (n - i >= 3) Console.Write(", ");
+        }
+        PrintNumbers3(i + 1, n);
+    }
+}
+
+void textTask()
+{Console.Clear(); Console.ForegroundColor = ConsoleColor.DarkYellow; Console.WriteLine(new string('_', Console.WindowWidth)); 
+Console.WriteLine("Знакомство с языками программирования. Семинар 9.\n" + 
+"\nЗадача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа, кратные 3-ём в промежутке от M до N.\n" +
+"M = 1; N = 9. -> 3, 6, 9 \n" + "M = 13; N = 20. -> 15, 18 \n");
+Console.WriteLine(new string('_', Console.WindowWidth) + "\n"); Console.ResetColor();
+}
     
